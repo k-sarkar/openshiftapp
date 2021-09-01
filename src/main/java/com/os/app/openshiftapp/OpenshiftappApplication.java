43f00946c60d3b9d1526aaad77d5c117dd713a36
Koushik Sarkar <koushik@li-5bcacd4c-2546-11b2-a85c-a3ae160187e4.ibm.com>
2021-08-31 21:10:05 -0500

    initial checkin

---
+++ src/main/java/com/os/app/openshiftapp/OpenshiftappApplication.java
@@ -0,0 +1,13 @@
+package com.os.app.openshiftapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftappApplication.class, args);
+	}
+
+}
